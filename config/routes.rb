0521514c700a16bb5b2af8af9691fe2b12ba5608
shreekshitha <shreekshitha@gmail.com>
2017-02-15 12:17:50 +0530

    Comment links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :comments
   devise_for :users
-  resources :links
+  resources :links do
+  	member do 
+  		put "like", to:"links#upvote"
+  		put "dislike", to:"links#downvote"
+  	end
+  	resources :comments
+  end
 
 root "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
